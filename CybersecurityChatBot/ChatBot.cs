30b53992ee4744c84061353bf85fa1801392a68d
hletelo24 <st10479122@rcconnect.edu.za>
2026-03-26 13:50:43 +0200

    Update ChatBot.cs

--- CybersecurityChatBot/ChatBot.cs
+++ CybersecurityChatBot/ChatBot.cs
@@ -8,5 +8,20 @@ namespace CybersecurityChatBot
 {
     internal class ChatBot
     {
+        public string UserName { get; set; }
+
+        private void PlayGreeting()
+        {
+            try
+            {
+                //Use of SoundPlayer to play a greeting audio file
+                SoundPlayer player = new SoundPlayer("repos\\CyberSecurityChatbot\\greeting.wav");
+                player.PlaySync();
+            }
+            catch
+            {
+                Console.WriteLine("Audio file missing");
+            }
+        }
     }
 }
